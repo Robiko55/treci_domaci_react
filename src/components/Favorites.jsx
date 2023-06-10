f292b22d7e43a0527b6a021cfa03683ee2bfbd6c
Aleksandar Matovic <am20190138@student.fon.bg.ac.rs>
2023-06-10 23:55:06 +0200

    Kreirani Favorites,Footer,Navbar i MovieList

---
+++ src/components/Favorites.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../App';
+const Navbar = () => {
+    return (
+       
+            <div>
+                Favorites
+                </div>
+       
+    );
+};
+
+export default Navbar
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../App';
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div>
+                <h1> React Books App</h1>
+                </div>
+            <div> Your Favourites </div>
+        </div>
+    );
+};
+
+export default Navbar
